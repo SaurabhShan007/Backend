d76afe015ab73dba79d7bf4008889a19eadaec97
Saurabh Chaudhary <139100313+SaurabhShan007@users.noreply.github.com>
2025-04-21 14:28:08 +0530

    updates01.1

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Lesson:1
+/*
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
@@ -12,3 +14,61 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+ */
+
+// Lesson:1 ends here
+
+// Lesson:2
+
+//Lesson:3 fs and path Modules
+const fs = require("fs");
+console.log(fs);
+console.log("starting");
+// fs.writeFileSync("saurabh.txt", "Hey there this is your Creative Developer, Saurabh")           //or the correct approach will be
+  fs.writeFile("saurabh2.txt", "Hey there this is Saurabh Chaudhary", ()=>{
+    console.log("done")
+    fs.readFile("saurabh2.txt",(error,data)=>{
+        console.log(error,data.toString())
+    })
+})  
+fs.appendFile("saurabh3.txt", "Hehehe you have successfully append the file",(e , d)=>{
+    console.log(d)
+})
+console.log("ending");
+
+//Example of callback hell
+/*
+fs.writeFile("saurabh2.txt", "Hey there this is Saurabh Chaudhary", () => {
+  console.log("done");
+  fs.readFile("saurabh2.txt", (error, data) => {
+    console.log(error, data.toString());
+    //again
+    fs.writeFile("saurabh2.txt", "Hey there this is Saurabh Chaudhary", () => {
+      console.log("done");
+      fs.readFile("saurabh2.txt", (error, data) => {
+        console.log(error, data.toString());
+        //again
+        fs.writeFile(
+          "saurabh2.txt",
+          "Hey there this is Saurabh Chaudhary",
+          () => {
+            console.log("done");
+            fs.readFile("saurabh2.txt", (error, data) => {
+              console.log(error, data.toString());
+              //again
+              fs.writeFile("saurabh2.txt", "Hey there this is Saurabh Chaudhary", ()=>{
+                console.log("done")
+                fs.readFile("saurabh2.txt",(error,data)=>{
+                    console.log(error,data.toString())
+            //again
+            
+                })
+            })  
+            });
+          }
+        );
+      });
+    });
+  });
+});
+*/
